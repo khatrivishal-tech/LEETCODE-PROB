9e670cb541fec5b192581679f7077dc49583cb5b
Vishal Khatri <khatrivishalk@gmail.com>
2026-08-19 19:09:56 +0530

    Time: 2 ms (13.83%), Space: 46.9 MB (46.27%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> ans = new ArrayList<>(n);
+        for(int i=0;i<n;i++){
+            if((i+1)%3==0&&(i+1)%5==0){
+                ans.add("FizzBuzz");
+            }
+            else if((i+1)%3==0){
+                ans.add("Fizz");
+            }
+            else if((i+1)%5==0){
+                ans.add("Buzz");
+            }
+            else 
+                ans.add(Integer.toString(i+1));
+        }
+        return ans;
+    }
+}
